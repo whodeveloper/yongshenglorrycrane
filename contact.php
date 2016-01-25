b91dbd3212c1f0ae1ad3a5fae7f5011ebad941bd
whodeveloper <whodevelopthis@gmail.com>
2016-01-26 00:51:54 +0800

    v1.0

---
+++ contact.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<?php include ("include/head.php");?>
+	</head>
+
+	<body>
+		<div class = "fullwrap">
+			<div class = "header">
+				<?php include ("include/header.php");?>
+			</div>
+			<div class = "navbar">
+				<?php include("include/navbar.php");?>
+			</div>
+
+			<div class = "maincontent">
+			</div>
+		</div>
+
+		<div class = "footer">
+			<?php include ("include/footer.php");?>
+		</div>
+	</body>
+</html>
+
+
+
+
+
---
+++ include/head.php
@@ -0,0 +1,4 @@
+<title>Yong Sheng Lorry Crane</title>
+<meta charset = "utf-8">
+<link rel = "stylesheet" type = "text/css" href = "css/style1.css">
+<link rel = "stylesheet" type = "text/css" href = "css/media.css">
